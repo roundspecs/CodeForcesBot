23592924cbd4dd72eb590d74159bf1633028a4ec
roundspecs <2502.zarif@gmail.com>
2022-11-15 05:48:27 +0600

    fix input and put in loop

--- utils.py
+++ utils.py
@@ -2,7 +2,18 @@ from typing import Callable, List
 
 
 def prompt(messsages: List[str], functions: List[Callable]):
-    print("Select an option:")
-    for i, message in enumerate(messsages, start=1):
-        print(f"{i}. {message}")
-    functions[i-1]()
+    while True:
+        print("Select an option:")
+        for i, message in enumerate(messsages, start=1):
+            print(f"{i}. {message}")
+        answer = input()
+        if answer.isdigit():
+            answer = int(answer)
+            if answer <= len(messsages):
+                functions[answer - 1]()
+                break
+        print(f"'{answer}' is not a valid option. Please try again.")
+
+
+if __name__ == "__main__":
+    prompt(["hello", "hi"], [lambda: print("hello"), lambda: print("hi")])
